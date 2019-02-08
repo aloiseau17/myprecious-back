fef588ba1e763018430e957d418cea60d76d053b
Audrey Loiseau <audrey.loiseau17@gmail.com>
2019-02-08 20:30:13 +0100

    Setup base laravel-cors: https://github.com/barryvdh/laravel-cors

---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+    // TODO restreindre au maximum
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
